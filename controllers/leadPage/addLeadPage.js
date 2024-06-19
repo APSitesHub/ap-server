dc1fb406d433a05d35c17ab93cac1a82bafa226b
Kyrylo Sulihan <106202828+PaulHighrow@users.noreply.github.com>
2024-06-19 17:04:12 +0300

    added log

--- controllers/leadPage/addLeadPage.js
+++ controllers/leadPage/addLeadPage.js
@@ -1,6 +1,7 @@
 const { newLeadPage } = require("../../services/leadPagesServices");
 
 const addLeadPage = async (req, res) => {
+  console.log(req);
   console.log("addPage", req.body);
   res.status(201).json(await newLeadPage(req.body));
 };
