d0e37cd470391eef56f6849e4e431ba7ea2c9f92
Kyrylo Sulihan <106202828+PaulHighrow@users.noreply.github.com>
2024-06-27 12:02:21 +0300

    test without contacts

--- middlewares/crm/updateQuizLead.js
+++ middlewares/crm/updateQuizLead.js
@@ -67,57 +67,57 @@ const updateQuizLead = async (req, res, next) => {
     ],
   };
 
-  const updateContactRequest = {
-    id: req.body.contactId,
-    name: req.body.name,
-    custom_fields_values: [
-      {
-        field_id: 1819781,
-        field_name: "Для кого навчання",
-        values: [
-          {
-            value: req.body.adult ? "Для себе" : "Для дитини",
-          },
-        ],
-      },
-      {
-        field_id: 1819779,
-        field_name: "Вік",
-        values: [
-          {
-            value: req.body.adult ? req.body.age : "",
-          },
-        ],
-      },
-      {
-        field_id: 1819821,
-        field_name: "Скільки років дитині",
-        values: [
-          {
-            value: req.body.adult ? "" : req.body.age,
-          },
-        ],
-      },
-      {
-        field_id: 1819783,
-        field_name: "Рівень мови",
-        values: [
-          {
-            value: req.body.adult ? req.body.knowledge : "",
-          },
-        ],
-      },
-      {
-        field_id: 1819823,
-        field_name: "Рівень англ дитини",
-        values: [
-          {
-            value: req.body.adult ? "" : req.body.knowledge,
-          },
-        ],
-      },
-    ],
-  };
+  // const updateContactRequest = {
+  //   id: req.body.contactId,
+  //   name: req.body.name,
+  //   custom_fields_values: [
+  //     {
+  //       field_id: 1819781,
+  //       field_name: "Для кого навчання",
+  //       values: [
+  //         {
+  //           value: req.body.adult ? "Для себе" : "Для дитини",
+  //         },
+  //       ],
+  //     },
+  //     {
+  //       field_id: 1819779,
+  //       field_name: "Вік",
+  //       values: [
+  //         {
+  //           value: req.body.adult ? req.body.age : "",
+  //         },
+  //       ],
+  //     },
+  //     {
+  //       field_id: 1819821,
+  //       field_name: "Скільки років дитині",
+  //       values: [
+  //         {
+  //           value: req.body.adult ? "" : req.body.age,
+  //         },
+  //       ],
+  //     },
+  //     {
+  //       field_id: 1819783,
+  //       field_name: "Рівень мови",
+  //       values: [
+  //         {
+  //           value: req.body.adult ? req.body.knowledge : "",
+  //         },
+  //       ],
+  //     },
+  //     {
+  //       field_id: 1819823,
+  //       field_name: "Рівень англ дитини",
+  //       values: [
+  //         {
+  //           value: req.body.adult ? "" : req.body.knowledge,
+  //         },
+  //       ],
+  //     },
+  //   ],
+  // };
 
   try {
     const currentToken = await getToken();
@@ -128,12 +128,12 @@ const updateQuizLead = async (req, res, next) => {
       `api/v4/leads/${req.body.crmId}`,
       updateRequest
     );
-    const contact = await axios.patch(
-      `api/v4/contacts/${req.body.contactId}`,
-      updateContactRequest
-    );
+    // const contact = await axios.patch(
+    //   `api/v4/contacts/${req.body.contactId}`,
+    //   updateContactRequest
+    // );
     console.log(crm.data);
-    console.log(contact.data);
+    // console.log(contact.data);
   } catch (error) {
     console.log(error);
     return res.status(400).json(error);
