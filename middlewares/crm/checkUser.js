6b5b4edc9fdea5589a19e2af2f714fea78373749
Kyrylo Sulihan <106202828+PaulHighrow@users.noreply.github.com>
2024-08-20 16:42:35 +0300

    userSchema fix

--- middlewares/crm/checkUser.js
+++ middlewares/crm/checkUser.js
@@ -5,7 +5,6 @@ const checkUser = async (req, _, next) => {
   try {
     const user = await findUserByID(req.params.id);
     req.body.user = user;
-    console.log(user);
   } catch (error) {
     console.log(error);
   }
