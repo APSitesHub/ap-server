cb26854533955a5ab11ddebac709d9f64796afd9
Kyrylo Sulihan <paulhighrow@gmail.com>
2024-10-31 18:29:44 +0200

    getPlatformToken middleware fix

--- middlewares/platform/getPlatformToken.js
+++ middlewares/platform/getPlatformToken.js
@@ -10,13 +10,16 @@ const getPlatformToken = async (req, res, next) => {
     return res.status(401).json("Login or password is wrong");
   }
 
-  console.log(user.pupilId);
+  if (user.pupilId === "0000000") {
+    console.log(14, "!trial user");
+    next();
+  }
 
   const updateLeadStatusRequest = {
     pupilId: user.pupilId,
   };
 
-  console.log(updateLeadStatusRequest);
+  console.log("getPlatformToken 22", updateLeadStatusRequest);
 
   try {
     axios.defaults.headers.common[
