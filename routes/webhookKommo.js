4a5759587601aa34fd54a61a676fd84e504b939c
Kyrylo Sulihan <106202828+PaulHighrow@users.noreply.github.com>
2024-11-08 17:45:15 +0200

    databaseObject in webhook logs

--- routes/webhookKommo.js
+++ routes/webhookKommo.js
@@ -8,6 +8,16 @@ router.post("/kommo", (req, res) => {
   if (req.body.leads.update[0].pipeline_id === "7001587") {
     console.log("Webhook received event from correct pipeline (7001587)!");
     console.log(req.body.leads.update[0]);
+    const databaseObject = req.body.leads.update[0];
+    console.log({
+      crmId: databaseObject.id,
+      name: databaseObject.name,
+      mail: databaseObject.custom_fields.find((field) => Object.keys(field).includes('Логін до платформи')).values[0].value,
+      password: databaseObject.custom_fields.find((field) => Object.keys(field).includes('Пароль до платформи')).values[0].value,
+      course: databaseObject.custom_fields.find((field) => Object.keys(field).includes('Потоки Річний курс')).values[0].value,
+      lang: databaseObject.custom_fields.find((field) => Object.keys(field).includes('Мова для вивчення')).values[0].value,
+      age: databaseObject.custom_fields.find((field) => Object.keys(field).includes('Скільки років?')).values[0].value,
+    });
   }
 
   return res.status(200).json({ message: "OK" });
