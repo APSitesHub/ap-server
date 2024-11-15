6e553c23b87a244257d453e79ace57161b43d2f4
illia Vodonis <illavodonis@gmail.com>
2024-11-15 16:12:05 +0200

    google sheets

--- routes/webhookKommo.js
+++ routes/webhookKommo.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const { newUser, findUser, updateUser } = require("../services/usersServices");
+const { google } = require("googleapis");
+const { getToken } = require("../services/tokensServices");
+const axios = require("axios");
 
 const router = express.Router();
 router.post("/kommo", async (req, res) => {
@@ -96,7 +99,79 @@ router.post("/kommo", async (req, res) => {
 });
 
 router.post("/google-sheets", async (req, res) => {
-  console.log(req.body);
+  console.log(req.body.leads.status);
+  const user = await getUserFromCRM(req.body.leads.status[0].id);
+  console.log("-----------------------------------");
+  console.log(user);
+  console.log("-----------------------------------");
+  const clientEmail = process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL;
+  const privateKey = process.env.GOOGLE_PRIVATE_KEY;
+  const googleSheetId = process.env.GOOGLE_SHEET_ID;
+  const googleSheetPage = "Аркуш3";
+
+  // authenticate the service account
+  const googleAuth = new google.auth.JWT(
+    clientEmail,
+    null,
+    privateKey.replace(/\\n/g, "\n"),
+    "https://www.googleapis.com/auth/spreadsheets",
+  );
+  try {
+    // google sheet instance
+    const sheetInstance = await google.sheets({
+      version: "v4",
+      auth: googleAuth,
+    });
+    // read data in the range in a sheet
+    const newRow = [[user.id, user.name, user.price, user.tags, user.custom]];
+
+    // Додаємо новий рядок у таблицю
+    await sheetInstance.spreadsheets.values.append({
+      spreadsheetId: googleSheetId,
+      range: googleSheetPage,
+      valueInputOption: "USER_ENTERED",
+      resource: {
+        values: newRow,
+      },
+    });
+
+    console.log("Новий рядок успішно додано");
+  } catch (err) {
+    console.log("readSheet func() error", err);
+  }
   return res.status(200).json({ message: "OK" });
 });
+
+async function getUserFromCRM(userID) {
+  const currentToken = await getToken();
+  console.log("userId", userID);
+
+  axios.defaults.headers.common["Authorization"] =
+    `Bearer ${currentToken[0].access_token}`;
+  const crmLead = await axios.get(`/api/v4/leads/${userID}?with=contacts`);
+
+  console.log("getLead data", crmLead.data);
+  const contactId = crmLead.data._embedded.contacts[0]?.id;
+  const crmContact =
+    contactId &&
+    (await axios.get(`/api/v4/contacts/${contactId}?with=contacts`));
+  console.log("getContact data", crmLead.data);
+  let tags = "";
+  crmLead.data._embedded.tags.forEach((tag, index) => {
+    console.log(index, tag);
+    tags = tags + tag.name + ", ";
+  });
+  console.log("custom field", crmLead.data.custom_fields_values);
+  const fieldData = crmLead.data.custom_fields_values.find((field) => {
+    return field.field_id === 557260;
+  });
+  return {
+    id: crmLead.data.id,
+    name: crmContact.data.name,
+    price: crmLead.data.price,
+    tags: tags,
+    custom: fieldData.values[0].value,
+  };
+}
+
 module.exports = router;
