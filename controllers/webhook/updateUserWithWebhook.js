cc63e36912c5ae9c42749d661497c42defa594ac
Kyrylo Sulihan <106202828+PaulHighrow@users.noreply.github.com>
2024-11-15 14:34:19 +0200

    empty values default values

--- controllers/webhook/updateUserWithWebhook.js
+++ controllers/webhook/updateUserWithWebhook.js
@@ -7,21 +7,23 @@ const {
 const updateUserWithWebhook = async (req, res) => {
   if (req.body.leads.update[0].pipeline_id === "7001587") {
     console.log("Webhook received event from correct pipeline (7001587)!");
-    console.log(req.body.leads.update[0]);
+    console.log(10, req.body.leads.update[0]);
     const databaseObject = req.body.leads.update[0];
     req.body.request = {
       crmId: +databaseObject.id,
       name: databaseObject.name,
-      mail: databaseObject.custom_fields.find((field) =>
-        Object.values(field).includes("Логін до платформи")
-      ).values[0].value,
-      password: databaseObject.custom_fields.find((field) =>
-        Object.values(field).includes("Пароль до платформи")
-      ).values[0].value,
+      mail:
+        databaseObject.custom_fields.find((field) =>
+          Object.values(field).includes("Логін до платформи")
+        ).values[0].value || "",
+      password:
+        databaseObject.custom_fields.find((field) =>
+          Object.values(field).includes("Пароль до платформи")
+        ).values[0].value || "",
       course: databaseObject.custom_fields
         .find((field) => Object.values(field).includes("Потоки Річний курс"))
         .values[0].value.slice(0, 3)
-        .replace(".", ""),
+        .replace(".", "") || '',
       lang:
         databaseObject.custom_fields.find((field) =>
           Object.values(field).includes("Мова для вивчення")
@@ -78,8 +80,8 @@ const updateUserWithWebhook = async (req, res) => {
           : "",
       age: databaseObject.custom_fields.find((field) =>
         Object.values(field).includes("Скільки років?")
-      ).values[0].value,
-      manager: +databaseObject.responsible_user_id,
+      ).values[0].value || '',
+      manager: +databaseObject.responsible_user_id || '',
     };
 
     if (+databaseObject.status_id === 58542315) {
