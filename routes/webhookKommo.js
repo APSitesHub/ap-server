03a78e6a32c5c029eb9419e115e761c4c0877b0d
Kyrylo Sulihan <106202828+PaulHighrow@users.noreply.github.com>
2024-11-21 17:55:54 +0200

    import fix

--- routes/webhookKommo.js
+++ routes/webhookKommo.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const updateUserWithWebhook = require("../controllers/webhook/updateUserWithWebhook");
 const getPlatformNumberAndPupilId = require("../middlewares/platform/getPlatformNumberAndPupilId");
+const getResponsibleUser = require("../middlewares/crm/getResponsibleUser");
 
 const router = express.Router();
 
-router.post("/kommo", getPlatformNumberAndPupilId, getResponsibleUser, updateUserWithWebhook);
+router.post(
+  "/kommo",
+  getPlatformNumberAndPupilId,
+  getResponsibleUser,
+  updateUserWithWebhook
+);
 
 module.exports = router;
