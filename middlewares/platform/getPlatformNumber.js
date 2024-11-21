63708cad1caac91a84816f456e032b7a09e99cd8
Kyrylo Sulihan <106202828+PaulHighrow@users.noreply.github.com>
2024-11-21 14:22:22 +0200

    more logs

--- middlewares/platform/getPlatformNumber.js
+++ middlewares/platform/getPlatformNumber.js
@@ -13,14 +13,21 @@ const getPlatformNumber = async (req, _, next) => {
         Object.values(field).includes("Логін до платформи")
       ).values[0].value;
 
+      console.log(16, login);
+
       if (login) {
         const marathonOne = await axios.get(
           `https://edvibe.com/school-api/api/Marathon/GetMarathonStudents?MarathonId=37835&SearchTerm=${login}`
         );
 
+        console.log(23, marathonOne);
+
         const marathonTwo = await axios.get(
           `https://edvibe.com/school-api/api/Marathon/GetMarathonStudents?MarathonId=49509&SearchTerm=${login}`
         );
+
+        console.log(29, marathonTwo);
+
         req.body.marathonNumber =
           marathonOne.data.length > 0
             ? "1"
@@ -30,7 +37,7 @@ const getPlatformNumber = async (req, _, next) => {
         next();
       }
     } catch (error) {
-      console.log(33, error);
+      console.log(33, 'error from getPlatformNumber', error);
       next();
     }
   }
