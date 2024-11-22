f76aad7effc0d6fb5b86971c94ad156105149c8e
illia Vodonis <illavodonis@gmail.com>
2024-11-22 15:02:55 +0200

    add google sheets integration

--- routes/webhookKommo.js
+++ routes/webhookKommo.js
@@ -2,6 +2,8 @@ const express = require("express");
 const updateUserWithWebhook = require("../controllers/webhook/updateUserWithWebhook");
 const getPlatformNumberAndPupilId = require("../middlewares/platform/getPlatformNumberAndPupilId");
 const getResponsibleUser = require("../middlewares/crm/getResponsibleUser");
+const getLeadsForGoogleSheets = require("../middlewares/crm/getLeadsCRM");
+const updateLeadPaidStatus = require("../middlewares/crm/updatePaidLeadStatus");
 
 const router = express.Router();
 
@@ -9,7 +11,9 @@ router.post(
   "/kommo",
   getPlatformNumberAndPupilId,
   getResponsibleUser,
-  updateUserWithWebhook
+  updateUserWithWebhook,
 );
+
+router.post("/google_kommo", getLeadsForGoogleSheets, updateLeadPaidStatus);
 
 module.exports = router;
