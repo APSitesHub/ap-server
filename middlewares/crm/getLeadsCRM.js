6844b46a1898c1350b3f9f16452c1c066ac3a666
illia Vodonis <illavodonis@gmail.com>
2024-11-22 15:03:47 +0200

    add google sheets integration

---
+++ middlewares/crm/getLeadsCRM.js
@@ -0,0 +1,41 @@
+const { getToken } = require("../../services/tokensServices");
+const axios = require("axios");
+
+const getLeadsForGoogleSheets = async (req, res, next) => {
+  try {
+    const SERVICE_PIPELINE_ID = 7001587;
+    const STATUS_WAIT_PAYMENT_ID = 71921048;
+
+    const currentToken = await getToken();
+    const { crmId } = req.body;
+
+    if (!crmId) {
+      return res.status(400).json({ error: "crmId is required" });
+    }
+
+    axios.defaults.headers.common["Authorization"] =
+      `Bearer ${currentToken[0].access_token}`;
+
+    const crmLead = await axios.get(
+      `https://apeducation.kommo.com/api/v4/leads/${crmId}`,
+    );
+
+    if (
+      crmLead.data &&
+      crmLead.data.pipeline_id === SERVICE_PIPELINE_ID &&
+      crmLead.data.status_id === STATUS_WAIT_PAYMENT_ID
+    ) {
+      return next();
+    }
+    return res.status(422).json({
+      error: `Lead with crmId ${crmId} does not have the allowed status`,
+    });
+  } catch (err) {
+    console.error("Error fetching lead:", err); // Логування помилки для дебагу
+    return res
+      .status(500)
+      .json({ error: `Error with updating lead status: ${err.message}` });
+  }
+};
+
+module.exports = getLeadsForGoogleSheets;
---
+++ middlewares/crm/updatePaidLeadStatus.js
@@ -0,0 +1,22 @@
+const { getToken } = require("../../services/tokensServices");
+const axios = require("axios");
+
+const updateLeadPaidStatus = async (req, res) => {
+  const STATUS_NEW_LEAD_ID = 58542315;
+  const currentToken = await getToken();
+  axios.defaults.headers.common["Authorization"] =
+    `Bearer ${currentToken[0].access_token}`;
+  try {
+    await axios.patch(
+      `https://apeducation.kommo.com/api/v4/leads/${req.body.crmId}`,
+      {
+        status_id: STATUS_NEW_LEAD_ID,
+      },
+    );
+    return res.status(200).json({ message: "OK" });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json(error);
+  }
+};
+module.exports = updateLeadPaidStatus;
