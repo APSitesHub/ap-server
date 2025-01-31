683d2fc1e5cdaa7b01ca862c66ab6bf689632fae
illia Vodonis <illavodonis@gmail.com>
2025-01-31 12:37:42 +0200

    add new webhook and logick for sales

--- routes/webhookAltegio.js
+++ routes/webhookAltegio.js
@@ -1,10 +1,7 @@
 const express = require("express");
 const router = express.Router();
-
+const altegioWebhook = require('../services/altegioWebhook')
 router.post(
-  "/test",(req, res) => {
-    console.log(req.body?.data.client)
-    res.status(200).json({ message: "OK" })
-  }
+  "/test", altegioWebhook
 );
 module.exports = router;
---
+++ services/botTelegram.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const TelegramBot = require('node-telegram-bot-api');
+
+const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN_AP_TECH_BOT, { polling: true });
+const ALLOWED_CHAT_ID = process.env.TELEGRAM_CHAT_ID_Notification_Proyobi_Sales;
+
+// Функція для надсилання повідомлення в чат
+const sendMessageToChat = (message, options = {}) => {
+
+  if (!ALLOWED_CHAT_ID) {
+    console.error('Chat ID is not set');
+    return;
+  }
+  bot.sendMessage(ALLOWED_CHAT_ID, message, { parse_mode: "Markdown", ...options });
+};
+
+module.exports = { bot, sendMessageToChat };
