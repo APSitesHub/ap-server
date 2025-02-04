b799b6d1bf10659e71218897ee2531e0aca8a408
illia Vodonis <illavodonis@gmail.com>
2025-02-04 12:52:07 +0200

    delete log

--- routes/trialLesson.js
+++ routes/trialLesson.js
@@ -9,11 +9,7 @@ const getTrialLessonInfo = require('../services/trialLesson');
 router.get('/:id', async (req, res) => {
     try {
         const id = parseInt(req.params.id, 10);
-        console.log(id);
         const response = await getTrialLessonInfo(id);
-        console.log('===========')
-        console.log(response)
-        console.log('===========')
         res.status(200).json({...response});
     } catch (err) {
         res.status(400).json();
