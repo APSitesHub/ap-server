e963984fd7e5f9e1559b23725ac7de4164f81fa3
illia Vodonis <illavodonis@gmail.com>
2025-05-06 19:40:56 +0300

    change import

--- controllers/leads/postPartnerLeadNMT.js
+++ controllers/leads/postPartnerLeadNMT.js
@@ -1,9 +1,9 @@
-const { createQuizLeadNMT } = require("../../services/crm/quizLeadNMT"); // Fix import to destructure the function
+const { createPartnerLeadNMT } = require("../../services/crm/partnerLeadNMT"); // Fix import to destructure the function
 
 const postPartnerLeadNMT = async (req, res) => {
     try {
         const data = req.body;
-        await createQuizLeadNMT(data);
+        await createPartnerLeadNMT(data);
         res.status(200).json({
             status: "success",
             message: "Lead created successfully",
